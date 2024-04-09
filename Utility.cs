0fb4c6150f033f87936bc5a3d51509ecb7608bf5
Eske Knudsen <elkn62304@edu.ucl.dk>
2024-04-09 13:12:26 +0200

    Utility class succesfull

--- Utility.cs
+++ Utility.cs
@@ -10,7 +10,22 @@ namespace Uge_15___Disaheim
     {
         public double GetValueOfBook(Book book)
         {
-            return;
+            return book.Price;
+        }
+
+        public double GetValueOfAmulet(Amulet amulet)
+        {
+            switch (amulet.Quality)
+            {
+                case Level.low:
+                    return 12.5;
+                case Level.medium:
+                    return 20.0;
+                case Level.high:
+                    return 27.5;
+                default:
+                    return 0;
+            }
         }
     }
 }
